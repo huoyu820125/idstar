27de31e4a965ee4af7859d38c004f278bc386a65
sunqian <sunqian820125@sina.com>
2020-05-29 10:21:35 +0800

    id-svr改名为example

---
+++ example/src/main/java/com/sq/example/InitIdStarBeans.java
@@ -0,0 +1,14 @@
+package com.sq.example;
+
+import com.sq.idstar.IdStarBeans;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Title InitIdStarBeans
+ * @Athor SunQian
+ * @CreateTime 2020/5/28 19:12
+ * @Description: todo
+ */
+@Configuration
+public class InitIdStarBeans extends IdStarBeans {
+}
